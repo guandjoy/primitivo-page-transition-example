55600072ff51b6ff857ca4c6db6f241c30f8fe3e
guandjoy <guandjoy@gmail.com>
2019-08-26 08:06:44 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,8 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-nprogress`,
+    `gatsby-plugin-transition-link`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
