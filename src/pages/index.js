118df59cc5ddc33830225b6e676ef3e49a1438be
guandjoy <guandjoy@gmail.com>
2019-08-28 23:29:00 +0300

    Add TransitionLink and TransitionEffect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,38 @@
 import React from "react"
 import { Link } from "gatsby"
+import TransitionLink from "gatsby-plugin-transition-link"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import TransitionEffect from "../components/TransitionEffect"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <TransitionEffect />
+      <TransitionLink
+        to="/page-2/"
+        onClick={() => console.log("clicked")}
+        exit={{
+          length: 2,
+          trigger: ({ exit }) => {
+            console.log("exit", exit)
+          },
+        }}
+        entry={{ delay: 0.5, length: 0, state: { layoutTheme: "dark" } }}
+      >
+        Go to page 2
+      </TransitionLink>
+    </Layout>
+  )
+}
 
 export default IndexPage
